a8cb8b5fd3514b86b7d5f20cd78e17aab1586fd5
quangthuyhoang <qtnhoang@gmail.com>
2018-03-05 19:29:51 -0600

    remove react logo div

--- src/App.js
+++ src/App.js
@@ -7,11 +7,6 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Powered by React</h1>
-        </header>
-        
         <Application />
       </div>
     );
